ba351b0c84f3ed432be0552d1e5fb3ddda720cc5
100495820 <159173134+100495820@users.noreply.github.com>
2024-02-27 23:05:20 +0100

    Add files via upload

---
+++ FINAL/myishere.c
@@ -0,0 +1,43 @@
+//P1-SSOO-23/24
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+	/* If less than three arguments (argv[0] -> program, argv[1] -> directory to search, argv[2] -> file to find) print an error y return -1 */
+	if(argc < 3)
+	{
+		printf("Too few arguments\n");
+		return -1;
+	}
+
+  DIR *dir;
+  struct dirent *entry;
+  int found = 0;
+
+  dir = opendir(argv[1]);
+
+  if (dir == NULL){
+    printf("Error: opening directory");
+    return -1;
+  }
+
+  while ((entry = readdir(dir)) != NULL && found != 1) {
+    if (strcmp(entry->d_name, argv[2]) == 0){
+      found = 1;
+    }
+  }
+  closedir(dir);
+  if (found == 1) {
+    printf("File %s is in directory %s\n", argv[2], argv[1]);
+  }
+  else {
+    printf("File %s is not in directory %s\n", argv[2], argv[1]);
+    }
+
+	return 0;
+}
---
+++ FINAL/myls.c
@@ -0,0 +1,41 @@
+//P1-SSOO-23/24
+
+#include <stdio.h>		// Header file for system call printf
+#include <unistd.h>		// Header file for system call gtcwd
+#include <sys/types.h>	// Header file for system calls opendir, readdir y closedir
+#include <dirent.h>
+#include <string.h>
+#include <limits.h>
+
+
+int main(int argc, char *argv[])
+{
+    struct dirent *entry;
+    DIR *dir;
+    char path[PATH_MAX];
+
+    if (argv[1] == NULL) {
+        if (getcwd(path, sizeof(path))!=NULL) {
+          dir = opendir(path);
+        }
+        else{
+          printf("Error: could not open current directory\n");
+          return -1;
+        }
+    } else {
+        dir = opendir(argv[1]);
+    }
+
+    if (dir == NULL) {
+        printf("Error: opening directory");
+        return -1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+
+    return 0;
+}
